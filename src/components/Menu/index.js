5502f1933f17cb40ac3b85df8f02641e45599412
Harun <harundogdu06@gmail.com>
2021-09-18 19:49:39 +0300

    we have activated the buttons.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import FormInput from "../shared-components/FormInput";
 import { Button } from "react-bootstrap";
+import { useCoffees } from "../../context/Coffee/CoffeeContext";
 
 function Menu() {
+  const { setCategory } = useCoffees();
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
   return (
     <div>
       <FormInput />
-      <Button className="d-block" title="All Coffees" variant="dark">
+      <Button
+        className="d-block"
+        title="All Coffees"
+        variant="dark"
+        value="all"
+        onClick={handleCategory}
+      >
         All Coffees
       </Button>
-      <Button className="d-block my-3" title="Hot" variant="danger">
+      <Button
+        className="d-block my-3"
+        title="Hot"
+        variant="danger"
+        value="hot"
+        onClick={handleCategory}
+      >
         Hot
       </Button>
-      <Button className="d-block" title="Iced" variant="info">
+      <Button
+        className="d-block"
+        title="Iced"
+        variant="info"
+        value="iced"
+        onClick={handleCategory}
+      >
         Iced
       </Button>
     </div>
--- src/context/Coffee/CoffeeContext.js
+++ src/context/Coffee/CoffeeContext.js
@@ -1,13 +1,31 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import coffees_data from "../../assets/db/coffees.json";
 
 export const CoffeeContext = createContext();
 
 export const CoffeeProvider = ({ children }) => {
   const [coffees, setCoffees] = useState(coffees_data);
+  const [category, setCategory] = useState("all");
+
+  useEffect(() => {
+    switch (category) {
+      case "hot":
+      case "iced":
+        setCoffees(
+          coffees_data.filter((coffee) => coffee.category === category)
+        );
+        break;
+      default:
+        setCoffees(coffees_data);
+        break;
+    }
+  }, [category]);
+
   const contextValues = {
     coffees,
     setCoffees,
+    category,
+    setCategory,
   };
   return (
     <CoffeeContext.Provider value={contextValues}>
