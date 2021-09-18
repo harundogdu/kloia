0e698b87156d0e4557bb34cb4bdbb243e130633f
Harun <harundogdu06@gmail.com>
2021-09-18 20:34:58 +0300

    a few design changes.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,12 +4,12 @@ import Content from "../Content";
 import { Col, Container as ContainerMain, Row } from "react-bootstrap";
 function Container() {
   return (
-    <ContainerMain className="mt-3">
+    <ContainerMain className="my-2">
       <Row>
-        <Col md={4}>
+        <Col md={4} className="border border-left">
           <Menu />
         </Col>
-        <Col md={8}>
+        <Col md={8}  className="border border-right">
           <Content />
         </Col>
       </Row>
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import { useCoffees } from "../../context/Coffee/CoffeeContext";
 function Content() {
   const { coffees } = useCoffees();
-  const arraySplit = (arr = Array) => {
-    return arr.map((i, key) => <span key={key}>{`${i} `}</span>);
+  const ingredientsSplit = (arr = Array) => {
+    return arr.map((item, key) => <span key={key}>{`${item} `}</span>);
   };
   return (
     <div>
       {coffees.map((coffee) => (
         <div key={`coffee_id_${coffee.id.toString()}`} className="mb-5">
           <h3 className="bold-text">{coffee.title}</h3>
-          <p className="bold-text">{coffee.description}</p>
+          <p>{coffee.description}</p>
           <span className="small">
             <strong>Ingredients: </strong>
-            {arraySplit(coffee.ingredients)}
+            {ingredientsSplit(coffee.ingredients)}
           </span>
         </div>
       ))}
