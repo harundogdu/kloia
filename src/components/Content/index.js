d694c99dfc46bd5e604c8a8224d7e1d8a9df2f2d
Harun <harundogdu06@gmail.com>
2021-09-18 19:28:18 +0300

    content area designed

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { useCoffees } from "../../context/Coffee/CoffeeContext";
 function Content() {
-  return <div>Content</div>;
+  const { coffees } = useCoffees();
+  const arraySplit = (arr = Array) => {
+    return arr.map((i, key) => <span key={key}>{`${i} `}</span>);
+  };
+  return (
+    <div>
+      {coffees.map((coffee, index) => (
+        <div key={`coffee_id_${index}`} className="mb-5">
+          <h3 className="bold-text">{coffee.title}</h3>
+          <p className="bold-text">{coffee.description}</p>
+          <span className="small">
+            <strong>Ingredients: </strong>
+            {arraySplit(coffee.ingredients)}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Content;
